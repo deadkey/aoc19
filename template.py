61086cdd556be174bb16d7c3fae713ad318bcc01
Maj Stenmark <maj.stenmark@cognibotics.com>
2019-12-08 10:06:22 +0100

    print in template

--- template.py
+++ template.py
@@ -5,10 +5,17 @@ from collections import *
 from main import run
 
 def p1(v, log=False):
+    lines = v.strip().split('\n')
     return 0
 
 def p2(v, log=False):
+    lines = v.strip().split('\n')
+    
     return 0
+
+def pp(*v):
+    if PP:
+        print(v) 
 
 def get_day():
     return date.today().day
@@ -16,5 +23,11 @@ def get_day():
 def get_year():
     return date.today().year
 
+
+
 if __name__ == '__main__':
-    run(get_year(), get_day(), p1, p2, D=True)
+    #0 samples_only, 1 run everything, 2 only my input data
+    DB = 0
+    #Debugprint: print if 1, not if 0
+    PP = 0
+    run(get_year(), get_day(), p1, p2, run_samples = DB < 2, samples_only = DB == 0)
