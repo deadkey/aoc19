26b7956d36c952e5c283f64a0adf09dacddda679
Maj Stenmark <maj.stenmark@cognibotics.com>
2019-12-09 07:55:02 +0100

    used the same intcode program in d7 as in d9

--- d7.py
+++ d7.py
@@ -12,28 +12,29 @@ from threading import Thread
 
 def get_cmd(v):
     lines = v.strip().split('\n')
-    cmd = []
+    cmd = defaultdict(int)
+    i = 0
     for line in lines:
         no = line.strip().split(',')
         for n in no:
 
             if len(n) > 0:
-                cmd.append(int(n))
+                cmd[i] = int(n)
+                i += 1
 
     return cmd
 
 
-def program(v, phase, to_use):
+def get(i, p, v, cmd):
+    if p[i] == 0:
+        return cmd[v[i]]
+    
+    return v[i]
+
+
+def program(cmd, phase, to_use):
     output = -1
     used = 0
-    lines = v.strip().split('\n')
-    cmd = []
-    for line in lines:
-        no = line.strip().split(',')
-        for n in no:
-
-            if len(n) > 0:
-                cmd.append(int(n))
     
     i = 0
     while True:
@@ -45,17 +46,28 @@ def program(v, phase, to_use):
         
         a = get(0, p, v, cmd)
         b = get(1, p, v, cmd)
+
         pos = v[2]
-        
+        if p[2] == 2:
+            pos += offset
         #ADD
         if code == 1:
+            
+            pp('add vals', p[0], v[0], p[1], v[1], p[2], v[2])
             cmd[pos] = a + b
             i += 4
+            pp('add', a, b, pos)
+            
         #MUL
         elif code == 2:
+
+            pp('mul vals', p[0], v[0], p[1], v[1])
             cmd[pos] = a * b
             i += 4
+
+            pp('mul', a, b, pos)
         #INPUT
+
         elif code == 3:
             a = 1
             #DIFFERENT POS!
@@ -134,23 +146,34 @@ def program2(cmd, ins, outputs, is_e = False):
         p = [int(opcode[2]), int(opcode[3]), int(opcode[4]), int(opcode[5])]
         N = len(cmd)
         v = [cmd[(i+1)%N], cmd[(i+2)%N], cmd[(i+3)%N], cmd[(i+4)%N]]
+        
         a = get(0, p, v, cmd)
         b = get(1, p, v, cmd)
+
         pos = v[2]
-        
         #ADD
         if code == 1:
+            
+            pp('add vals', p[0], v[0], p[1], v[1], p[2], v[2])
             cmd[pos] = a + b
             i += 4
+            pp('add', a, b, pos)
+            
         #MUL
         elif code == 2:
+
+            pp('mul vals', p[0], v[0], p[1], v[1])
             cmd[pos] = a * b
             i += 4
+
+            pp('mul', a, b, pos)
         #INPUT
         elif code == 3:
             #READS INPUT
             #DIFFERENT POS!
-            cmd[v[0]] = ins.get()
+            pos = v[0]
+
+            cmd[pos] = ins.get()
             i += 2
         #OUTPUT
         elif code == 4:
@@ -161,45 +184,46 @@ def program2(cmd, ins, outputs, is_e = False):
                 #print(a)
                 e_out = a
             i += 2
-        #JUMP IF NOT ZERO
+         #JUMP IF NOT ZERO
         elif code == 5:
-            
             if a != 0:
                 i = b
             else:
                 i += 3
             
+            pp('jmp', a, b)
+            
         #JUMP IF ZERO
         elif code == 6:
-            
             if a == 0:
                 i = b
+
             else:
 
                 i += 3
-        
-        #LESS
+        #LESS     
         elif code == 7:
-            
             if a < b:
                 cmd[pos] = 1
             else:
                 cmd[pos] = 0
             i += 4
 
+            pp('less', a, b)
+
         #EQ
         elif code == 8:
-
             if a == b:
                 cmd[pos] = 1
             else:
                 cmd[pos] = 0
             
             i += 4
-
-        #DONE
+            pp('eq', a, b)
+       
+        #DONE!
         elif code == 99:
-            return True
+            return outputs
 
         else:
             print('something went wrong', i)
@@ -209,12 +233,13 @@ def program2(cmd, ins, outputs, is_e = False):
     return True
 
 def test_phase(v, ph):
+    cmds= [get_cmd(v) for _ in range(5)]
 
-    a_out = program(v, [ph[0], 0], 2)
-    b_out = program(v, [ph[1], a_out], 2)
-    c_out = program(v, [ph[2], b_out], 2)
-    d_out = program(v, [ph[3], c_out], 1)
-    e_out = program(v, [ph[4], d_out], 1)
+    a_out = program(cmds[0], [ph[0], 0], 2)
+    b_out = program(cmds[1], [ph[1], a_out], 2)
+    c_out = program(cmds[2], [ph[2], b_out], 2)
+    d_out = program(cmds[3], [ph[3], c_out], 1)
+    e_out = program(cmds[4], [ph[4], d_out], 1)
     return e_out
                      
 
